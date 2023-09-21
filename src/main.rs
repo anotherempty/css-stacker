389011345f23d77416f58b78117fa00ef91db46f
anotherempty <anotherempty@outlook.com>
2023-09-21 13:36:10 +0300

    reorganized code

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,61 @@
-use std::{collections::HashSet, fs::File, io::Write, path::PathBuf};
+use std::{fs::File, io::Write, path::Path};
 
 use ignore::WalkBuilder;
-use lightningcss::{
-    stylesheet::{MinifyOptions, ParserOptions, PrinterOptions, StyleSheet},
-    targets::{Browsers, Targets},
-};
+use lightningcss::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
+use thiserror::Error;
 
 // assemble all css files
 // compile sass
 // process with lightningcss
 
+// options :
+// - ignore files and folders
+// - input and output file
+// - minification and prettiness
+// - browsers targets
+// - use gitignore
+// -
+
+const EXTENSIONS: [&str; 3] = ["scss", "css", "sass"];
+
+#[derive(Debug, Error)]
+enum ProcessError {
+    #[error("Style collecting error: {0}")]
+    StyleSearch(String),
+    #[error("Sass processing error: {0}")]
+    Sass(String),
+    #[error("Css processing error: {0}")]
+    Stylesheet(String),
+    #[error("File handling error: {0}")]
+    Writing(String),
+}
+
 fn main() {
-    let css = "./style.scss";
+    let styles = retrieve_styles("./").unwrap();
 
-    let allowed_extension = ["scss", "css"];
+    let sass = compile_sass(&styles).unwrap();
 
-    let ignored_files = [
-        "./style.css",
-        "./style.scss",
-        "./style.min.css",
-        "./style.min.scss",
-    ];
+    let css = sass_to_css(&sass, true).unwrap();
 
-    let mut file = File::create(css).unwrap();
+    write_styles("style", &css.0, css.1).unwrap();
+}
 
-    let mut sass = String::new();
+fn retrieve_styles<P>(path: P) -> Result<String, ProcessError>
+where
+    P: AsRef<Path>,
+{
+    let mut styles = String::new();
 
-    for result in WalkBuilder::new("./")
+    for result in WalkBuilder::new(path)
         .hidden(false)
-        .filter_entry(move |p| !ignored_files.contains(&p.clone().into_path().to_str().unwrap()))
+        .filter_entry(|p| {
+            !(p.path().parent().unwrap().to_str().unwrap() == "." && p.path().is_file())
+        })
         .build()
     {
         match result {
             Ok(entry) => {
-                println!("{}", entry.path().display());
-                if allowed_extension.contains(
+                if EXTENSIONS.contains(
                     &entry
                         .path()
                         .extension()
@@ -42,61 +63,104 @@ fn main() {
                         .to_str()
                         .unwrap_or_default(),
                 ) {
-                    println!(
-                        "ext: {}",
-                        entry.path().extension().unwrap().to_str().unwrap()
-                    );
+                    let path = entry.path().display();
 
-                    sass.push_str(&format!("@use '{}';\n", entry.path().display()));
+                    styles.push_str(&format!(
+                        "@use '{}' as {};\n",
+                        path,
+                        path.to_string()
+                            .replace('_', "-")
+                            .replace(['/', '.', '&'], "_")
+                            .chars()
+                            .filter(|c| c.is_ascii_alphanumeric() || *c == '-' || *c == '_')
+                            .collect::<String>()
+                    ));
                 }
             }
-            Err(err) => println!("ERROR: {}", err),
+            Err(err) => return Err(ProcessError::StyleSearch(err.to_string())),
         }
     }
-    file.write_all(sass.as_bytes()).unwrap();
 
-    file.flush().unwrap();
+    Ok(styles)
+}
 
-    let sass = grass::from_path(css, &grass::Options::default()).unwrap();
+fn compile_sass(styles: &str) -> Result<String, ProcessError> {
+    grass::from_string(styles, &grass::Options::default())
+        .map_err(|err| ProcessError::Sass(err.to_string()))
+}
 
-    let mut stylesheet = StyleSheet::parse(&sass, ParserOptions::default()).unwrap();
+fn sass_to_css(styles: &str, minify: bool) -> Result<(String, Option<String>), ProcessError> {
+    let stylesheet = StyleSheet::parse(styles, ParserOptions::default())
+        .map_err(|err| ProcessError::Stylesheet(err.to_string()))?;
 
-    let targets = Targets {
-        browsers: Some(Browsers {
-            chrome: Some(80),
-            firefox: Some(70),
-            edge: Some(80),
-            safari: Some(13),
-            opera: Some(67),
-            ..Default::default()
-        }),
-        ..Default::default()
-    };
+    // * doesn't seem to do any structural modification : minification, vendor prefixes, etc
+    // stylesheet
+    //     .minify(MinifyOptions {
+    //         targets: Targets {
+    //             browsers: Some(Browsers {
+    //                 chrome: Some(50),
+    //                 edge: Some(12),
+    //                 ie: Some(10),
+    //                 safari: Some(12),
+    //                 opera: Some(40),
+    //                 firefox: Some(50),
+    //                 ..Default::default()
+    //             }),
+    //             ..Default::default()
+    //         },
+    //         ..Default::default()
+    //     })
+    //     .unwrap();
 
-    stylesheet
-        .minify(MinifyOptions {
-            targets,
-            unused_symbols: HashSet::new(),
-        })
-        .unwrap();
-
-    let res = stylesheet
+    let css = stylesheet
         .to_css(PrinterOptions {
-            minify: true,
-            source_map: None,
-            project_root: None,
-            targets,
-            analyze_dependencies: None,
-            pseudo_classes: None,
+            minify: false,
+            ..Default::default()
         })
-        .unwrap();
+        .map_err(|err| ProcessError::Stylesheet(err.to_string()))?;
 
-    println!("{}", res.code);
+    let styles = css.code;
 
-    let file_path = PathBuf::from("./style.css");
+    let mut styles_min = None;
 
-    let mut file = File::create(file_path).unwrap();
+    if minify {
+        let css_min = stylesheet
+            .to_css(PrinterOptions {
+                minify: true,
+                ..Default::default()
+            })
+            .map_err(|err| ProcessError::Stylesheet(err.to_string()))?;
 
-    file.write_all(res.code.as_bytes()).unwrap();
-    file.flush().unwrap();
+        styles_min = Some(css_min.code);
+    }
+
+    Ok((styles, styles_min))
+}
+
+fn write_styles(
+    filename: &str,
+    styles: &str,
+    styles_min: Option<String>,
+) -> Result<(), ProcessError> {
+    let mut file = File::create(format!("{filename}.css"))
+        .map_err(|err| ProcessError::Writing(err.to_string()))?;
+
+    file.write_all(styles.as_bytes())
+        .map_err(|err| ProcessError::Writing(err.to_string()))?;
+
+    file.flush()
+        .map_err(|err| ProcessError::Writing(err.to_string()))?;
+
+    if let Some(styles_min) = styles_min {
+        let mut file = File::create(format!("{filename}.min.css"))
+            .map_err(|err| ProcessError::Writing(err.to_string()))?;
+
+        file.write_all(styles_min.as_bytes())
+            .map_err(|err| ProcessError::Writing(err.to_string()))?;
+
+        file.flush()
+            .map_err(|err| ProcessError::Writing(err.to_string()))?;
+    }
+
+    Ok(())
 }
