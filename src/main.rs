21dc17a533d8f53ef591bd10d5eed8e8647224fa
anotherempty <anotherempty@outlook.com>
2023-09-21 16:18:32 +0300

    implemented cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use clap::Parser;
 use css_stacker::stack_styles;
 
+/// Simple program to stack css|scss|sass files into a single file
+#[derive(Parser, Debug)]
+#[command(version)]
+pub struct Args {
+    /// Path to the directory containing the styles
+    #[arg(short, long, default_value = "./")]
+    path: String,
+
+    /// Name with path of the output css file without the extension
+    #[arg(short, long, default_value = "./style")]
+    output: String,
+
+    /// Whether to include the files that are the direct children of the provided path directory
+    #[arg(short, long, default_value = "false")]
+    minify: bool,
+
+    /// Whether to include the files that are the direct children of the provided path directory
+    #[arg(short, long, default_value = "false")]
+    include_path_styles: bool,
+}
+
 fn main() {
-    stack_styles("./", "style", false, false);
+    let args = Args::parse();
+
+    println!("{:?}", args);
+
+    stack_styles(
+        args.path,
+        args.output,
+        args.minify,
+        args.include_path_styles,
+    );
 }
