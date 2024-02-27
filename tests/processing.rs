5553bfec8fa520cd1e9cbca5ba945dbe8f6d20e9
anotherempty <anotherempty@outlook.com>
2024-02-27 14:51:36 +0300

    Update test

--- tests/processing.rs
+++ tests/processing.rs
@@ -1,10 +1,10 @@
 use std::{fs::File, io::Read};
 
-use css_stacker::{stack_styles, Format};
+use css_stacker::Stacker;
 
 #[test]
 fn test_stacking() {
-    stack_styles("./tests", "./tests/test", Format::Both);
+    let _ = Stacker::create("./tests", "./tests/test", None);
 
     let test_result = "body{color:#000;background:#fff}body>p{color:#000}div{border:1px solid #000}a{color:red;transition:all .15s ease-in-out}.color{color:red;padding:1rem}.color>span{color:#00f}";
 
